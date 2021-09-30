bff889b5ab45fc81c72560f4dd6041f813ebcfff
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-01 01:55:38 +0200

    created header game view + footers for multiplayer and solo

---
+++ src/components/game/footers/FooterMulti.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="footer">
+    <div class="wrapper" v-for="(player, index) in numberOfPlayers" :key="index">
+      <div class="player active">
+        <h3>P{{ index }}</h3>
+        <p class="score">4</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterMulti",
+  computed: {
+    numberOfPlayers() {
+      return this.$store.state.numberOfPlayers;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#footer {
+  display: flex;
+  flex-direction: row;
+  gap: 1em;
+
+  .wrapper {
+    flex: 1;
+    text-align: center;
+
+    .player {
+      background-color: var(--clr-secondary);
+      border-radius: 0.3125rem;
+      padding: 1em 0;
+
+      h3 {
+        margin-bottom: 0.5em;
+      }
+
+      p {
+        font-size: 1.5em;
+      }
+    }
+
+    .active {
+      background-color: var(--clr-primary);
+      color: var(--clr-white);
+
+      h3 {
+        color: var(--clr-white);
+      }
+    }
+  }
+}
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,13 +1,77 @@
 <template>
-    <div>
-        <h1>Game</h1>
+  <div id="game" class="container">
+    <div class="header">
+      <h1>memory</h1>
+      <button class="btn btn-primary">Menu</button>
     </div>
+    <div class="game"></div>
+    <FooterMulti />
+    <FooterSolo />
+  </div>
 </template>
 
 <script>
+import FooterMulti from "../components/game/footers/FooterMulti.vue";
+import FooterSolo from "../components/game/footers/FooterSolo.vue";
+
 export default {
-    name: "Game",
+  name: "Game",
+  components: { FooterMulti, FooterSolo },
+  created() {
+    this.generateCards();
+  },
+  computed: {
+    selectedTheme() {
+      return this.$store.state.theme;
+    },
+    selectedNumberOfPlayers() {
+      return this.$store.state.numberOfPlayers;
+    },
+    selectedGridSize() {
+      return this.$store.state.gridSize;
+    },
+  },
+  methods: {
+    generateCards() {
+      let maxNumbers = 20;
+      let maxArray = 8;
+      let numbersArray = [];
+
+      for (let i = 0; i < maxArray; i++) {
+        let randomNumber = Math.floor(Math.random() * maxNumbers);
+        numbersArray.push(randomNumber);
+      }
+
+      numbersArray = [...numbersArray, ...numbersArray];
+      console.log(numbersArray);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#game {
+  //height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 1em;
+  margin-bottom: 1em;
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .btn {
+      padding: 0.5em 1.25em;
+    }
+  }
+
+  .footer {
+    display: flex;
+    flex-direction: row;
+    gap: 1em;
+  }
+}
+</style>

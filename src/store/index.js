4703cd5f48a9041cb91a8c9cf1da66985629f137
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-09-30 22:33:35 +0200

    setup project

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,22 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state: {
+        theme: "numbers",
+        numberOfPlayers: 1,
+        gridSize: "4x4",
+    },
+    mutations: {
+        startGame(state, payload) {
+            state.theme = payload.theme;
+            state.numberOfPlayers = Number(payload.numberOfPlayers);
+            state.gridSize = payload.gridSize;
+        },
+    },
+    actions: {
+        startGame({ commit }, payload) {
+            commit("startGame", payload);
+        },
+    },
+    modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="home">
+        Home
+        <button class="btn btn-primary" type="submit" @click="startGame">
+            Start Game
+        </button>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+    name: "Home",
+    components: {},
+    methods: {
+        startGame() {
+            this.$router.push("/game");
+        },
+    },
 };
 </script>
+
+<style lang="scss" scoped></style>
